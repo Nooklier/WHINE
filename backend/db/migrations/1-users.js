844604351c4f8ab21f96f846385c91c0a7843adf
Nooklier <muenseepromn@gmail.com>
2024-04-18 06:30:46 -0700

    added options in migrations

--- backend/db/migrations/1-users.js
+++ backend/db/migrations/1-users.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
@@ -32,7 +37,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Users");
